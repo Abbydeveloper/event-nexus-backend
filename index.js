ccfc7bfa363778540ad3ea05490e32b0f27cfbd8
Adeboga Abigail <bogadeji@gmail.com>
2025-02-16 02:30:18 +0100

    Setup database using neon serverless

--- index.js
+++ index.js
@@ -16,6 +16,16 @@ const catchAsync = require('./utils/catchAsync');
 const app = express();
 
 
+const { neon } = require("@neondatabase/serverless");
+
+const sql = neon(process.env.DATABASE_URL);
+
+const requestHandler = async (req, res) => {
+  const result = await sql`SELECT version()`;
+  const { version } = result[0];
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end(version);
+};
 
 const corsOptions ={
     origin:'https://event-nexus-frontend.vercel.app', 
