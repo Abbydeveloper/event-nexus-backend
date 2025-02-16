e6bbaaaf4480861381d8bee02687c0e273d804ed
Adeboga Abigail <bogadeji@gmail.com>
2025-02-16 03:31:13 +0100

    Fix database connection

--- config/config.js
+++ config/config.js
@@ -17,11 +17,8 @@ module.exports = {
     dialect: "mysql"
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres"
+    url: process.env.DATABASE_URL,
+    dialect: "postgres",
+    dialectOptions: { ssl: { require: true } },
   }
 }
