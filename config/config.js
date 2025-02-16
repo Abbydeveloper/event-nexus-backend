10b55a5a1670df409380b6204d8077423d5d17c4
Adeboga Abigail <bogadeji@gmail.com>
2025-02-16 04:19:56 +0100

    Trouble db connection error

--- config/config.js
+++ config/config.js
@@ -13,7 +13,7 @@ module.exports = {
     username: "root",
     password: null,
     database: "database_test",
-    host: "127.0.0.1",
+    host: "",
     dialect: "mysql"
   },
   production: {
--- index.js
+++ index.js
@@ -38,6 +38,9 @@ app.use(express.json());
 app.use(morgan('dev'));
 
 // app routes
+app.use('/', () => {
+  console.log('Here now')
+})
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/events', eventRouter)
 app.use('/api/v1/users', userRouter)
